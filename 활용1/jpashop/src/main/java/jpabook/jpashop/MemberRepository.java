44023874c4b86e620c4899fb56a70a9cd06b732e
boraborason <57280699+boraborason@users.noreply.github.com>
2020-12-04 00:34:48 +0900

    MemberRepositoryTest 오류해결
    MVCC=TRUE 옵션제거

--- 활용1/jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
+++ 활용1/jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-@Repository //자동 스프링빈 등록 @autowired로 자동주입가능
+@Repository //자동 스프링빈 등록해줌 @autowired로 자동주입가능
 public class MemberRepository {
 
     @PersistenceContext // 스프링 부트가 엔티티매니저 자동생성
@@ -13,7 +13,7 @@ public class MemberRepository {
 
     //저장하는 메서드
     public Long save(Member member){
-        em.persist(member);
+        em.persist(member);  //EntityManager에 멤버저장 - 테이블에 저장
         return member.getId(); //아이디만 반환해서 조회에 사용
     }
 
--- 활용1/jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ 활용1/jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -9,16 +9,17 @@ import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@Transactional
+
 @SpringBootTest //스프링부트로 테스트
 public class MemberRepositoryTest
 {
     @Autowired //각 상황에 맞는 loc컨테이너에 존재하는 bean을 자동으로 주입해준다.
-    MemberRepository memberRepository;
+    MemberRepository memberRepository; //인젝션 받음
 
+    @Transactional //test에 있을 시 롤백해버려서 db에는 남지않음
     @Test
      //엔티티매니저를 통한 데이터 변경은 transaction을 필수 거쳐야 한다.
-    //	@Rollback(false)	테스트 종료 후 데이터를 롤배하지 않고 그대로 남겨두는 옵션
+    @Rollback(false)	//테스트 종료 후 데이터를 롤배하지 않고 그대로 남겨두는 옵션
     public void testMember() throws Exception
     {
         //given
@@ -30,12 +31,12 @@ public class MemberRepositoryTest
         Member findMember = memberRepository.find(savedId);
 
         //then
-//        assertEquals(findMember.getId(), member.getId());
-//        assertEquals(findMember.getUsername(), member.getUsername());
-//        assertEquals(findMember, member);
+        assertEquals(findMember.getId(), member.getId());
+        assertEquals(findMember.getUsername(), member.getUsername());
+        assertEquals(findMember, member);
 
-        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+//        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+//        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
 
         System.out.println("findMember == member : " + (findMember == member));
         //같은 트랜젝션 안에서 아이디 값이 같으면 같은 엔티티로 식별됨 (기본편 보충)
